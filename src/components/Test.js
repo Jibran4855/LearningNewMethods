764293750b8f04519d5a69f55ee6c4daf9f3c787
mrbilalashraf <bilalashrafme@gmail.com>
2023-10-02 15:03:19 +0500

    Commit in Test.js

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,7 +1,16 @@
-const Test = ()=><div style={{display:"flex", justifyContent:"space-between", width:"100%", background:"red"}}>
-<p>Hello</p>
-<p>Test here</p>
-</div>
+const Test = () => (
+  <div
+    style={{
+      display: "flex",
+      justifyContent: "space-between",
+      width: "100%",
+      background: "red",
+    }}
+  >
+    <p>Hello</p>
+    <p>Test here</p>
+    <p>Hello from bilal</p>
+  </div>
+);
 
-
-export default Test
+export default Test;
